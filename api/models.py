3d30ab1f7912faaadb4f65b54e69d54ebdbaddd5
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2024-01-07 21:29:17 +0300

    feat: add categories toString transformation

--- api/models.py
+++ api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100, blank=False, default='')
     
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name_plural = 'categories'
 
