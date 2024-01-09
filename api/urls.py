e2bcc28fec82b78a19b2ca029e635772f8c2a09a
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2024-01-09 20:06:55 +0300

    minor: correct url mistake

--- api/urls.py
+++ api/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('posts/', views.PostList.as_view()),
     path('posts/<int:pk>/', views.PostDetail.as_view()),
     path('comments/', views.CommentList.as_view()),
-    path('commetns/<int:pk>/', views.CommentDetail.as_view())
+    path('comments/<int:pk>/', views.CommentDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
