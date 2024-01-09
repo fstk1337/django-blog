800cf7ce4880063013318e3c7fb4401f9362b229
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2024-01-09 22:15:03 +0300

    feat: add favicon file

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig',
+    'api.apps.ApiConfig'
 ]
 
 MIDDLEWARE = [
--- blog/urls.py
+++ blog/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls'))
+    path('api/v1/', include('api.urls')),
+    path('favicon.ico', RedirectView.as_view(url='/media/favicon.ico', permanent=True))
 ]
 
 if settings.DEBUG:
